0ef3403f82c1e22a478a01e946aee0d37c0a8293
Adrian <adense13@gmail.com>
2015-09-22 15:37:57 +0200

    Database dump
    -Create new account under "Privileges"
    -Username "admin", password "admin", host "localhost
    
    grant all privileges on webshop12database.* to admin'@'localhost'
    identified by 'admin' ; flush privileges;

--- webshop12database.sql
+++ webshop12database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2015 at 07:25 PM
+-- Generation Time: Sep 22, 2015 at 03:34 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `nbrInStore`, `imageURL`, `price`) VALUES
-(1, 'Pizza', 'This tasty piece of food can be eaten.', 44, '', 75),
+(1, 'Pizza', 'This tasty piece of food can be eaten.', 20, '', 75),
 (2, 'Computer', 'A gaming rig of never before seen powerful components.', 2, '', 10000),
-(3, 'Pen', 'A pen for drawing. Works forever.', 5332, '', 10),
-(4, 'Cookie', 'A krusty cookie.', 22, '-', 16),
-(5, 'Thing', 'A mysterious object.', 244, '-', 199);
+(3, 'Pen', 'A pen for drawing. Works forever.', 5323, '', 10),
+(4, 'Cookie', 'A krusty cookie.', 21, '-', 16),
+(5, 'Thing', 'A mysterious object.', 242, '-', 199);
 
 -- --------------------------------------------------------
 
@@ -66,8 +66,9 @@ INSERT INTO `products` (`id`, `name`, `description`, `nbrInStore`, `imageURL`, `
 
 CREATE TABLE IF NOT EXISTS `users` (
   `userName` varchar(15) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `homeAddress` varchar(50) NOT NULL,
+  `nbrFailedLogin` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -75,8 +76,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userName`, `password`, `homeAddress`) VALUES
-('admin', 'admin', 'admin');
+INSERT INTO `users` (`userName`, `password`, `homeAddress`, `nbrFailedLogin`) VALUES
+('admin', '$2y$10$yoPo9hfwtMDbrOlqLMiKcOiEHgMztYbL1u0opX7yzdPnavGs3p9hK', 'Lund', 0),
+('hash', '$2y$10$xYTxN1do1h8XqOGWZ8OCje8.PxssiCf.3Ly6x5f3lcEalB5hgtzYS', 'Lund', 0),
+('minh', '$2y$10$n3WkQUanA55.s4mTUaJ3SOQvPhenG.I0JVmC.Z89eQT.PMk0HssPa', 'Helsingborg', 0),
+('test', '$2y$10$KXIwQf6S85v0kh6XFKCGU.5QYHVz5kHkPN/ZZRlTwgvrE4q4iyPzm', 'Lund', 0),
+('test3', '$2y$10$dIyFz0rM6p17rM.Kc/r2OuUCPaThQab8mlMX3wU7z2XLdw15vzvIa', 'Lund', 0),
+('test4', '$2y$10$DmGhcftJGscSZ3xzyecxI.SFGfvAuH.9A2X6OVINfctEfHTaTNyh6', 'Lund', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
